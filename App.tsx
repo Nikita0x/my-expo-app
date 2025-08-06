e1a7e507fabafe1aa09be9132a1897abbe2984d3
Nikita0x <klirmio21@gmail.com>
2025-08-06 20:53:50 +0300

    added: necessary packages for hot reloading working in web and tailwind

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
+import '@expo/metro-runtime';
+import './global.css';
+
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import { Text } from 'react-native';
 
 export default function App() {
   return (
     <>
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
+      <Text className="text-[90px] text-green-500">Hello how are you</Text>
+      <Text className="bg-red-300 text-[50px] text-purple-500">Hel1111lo</Text>
+      <Text>Helawgawgalo</Text>
+      <Text>Hello</Text>
+      <Text>Helwagawglo</Text>
+      <Text>Hellogawg</Text>
+      <Text>Helgawglo</Text>
+      <Text>Hello</Text>
     </>
   );
 }
